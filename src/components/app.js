60ddb9baba34d1a3322f7be1441c11bf9fff8581
Thomas PERAY <thomas.peray@etu.univ-nantes.fr>
2021-03-09 16:38:59 +0100

    1.1 : App de base fonctionnelle avec Vue

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+Vue.component('app', {
+    template: `<div id="app"> 
+                    <h1>Voilà c'est le site</h1>
+               </div>`,
+    data : function () {
+        return {
+
+        }},
+    methods: {
+
+    }
+})
--- src/spoonacular.js
+++ src/spoonacular.js
@@ -2,5 +2,28 @@ const API_KEY = '805f9224f7a34732a3ffd6f68ce31d5e';
 
 const useCuisineApi = {
 
+    /*
+        Cherche par ingrédients à partir d'un tableau d'ingrédients
+        Le tableau doit contenir au moins 1 ingrédients
+        Exemple : ["apples","flour",sugar"]
+        // Exemple de requete : https://api.spoonacular.com/recipes/findByIngredients?apiKey=805f9224f7a34732a3ffd6f68ce31d5e&ingredients=apples,+flour,+sugar.
+     */
+    byIngredient: (ingredients) => new Promise((resolve, reject) => {
+        let ingredientUrlRequest= `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${API_KEY}&ingredients=`;
+        ingredientUrlRequest += ingredients[0]
+        ingredients.shift();
+        ingredients.forEach(ingr => {
+            ingredientUrlRequest += ",+" + ingr;
+        });
+        fetch(ingredientUrlRequest).
+        then((response) => response.json()).
+        then(data => {
+            resolve(data);
+        }).
+        catch(error => reject(error));
+    })
+
 };
 export default useCuisineApi;
+
+
